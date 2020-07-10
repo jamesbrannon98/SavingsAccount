00dc6ef7775fc424c354d726ba84c4ae724fa3ac
jamesbrannon98 <jimmybrannon@comcast.net>
2020-07-09 20:44:51 -0400

    Added comments to main.

--- src/main/java/SavingsAccount/SavingsAccount.java
+++ src/main/java/SavingsAccount/SavingsAccount.java
@@ -23,12 +23,13 @@ public class SavingsAccount {
     }
     
     public static void main(String args[]){
+        /* Sets the balance of two savings accounts as well as the interest rate. */
         SavingsAccount saver1 = new SavingsAccount(2000);
         SavingsAccount saver2 = new SavingsAccount(3000);
         SavingsAccount.setInterestRate(0.04);
         System.out.println("Savings Account Balances");
         System.out.printf("%-10s%10s%10s\n", "Month", "Saver1", "Saver2");
-        
+        /* Calculates interest over 12 months. */
         for(int month = 1; month <= 12; month++){
             String monthNum = Integer.toString(month);
             saver1.calculateMonthlyInterest();
@@ -36,7 +37,7 @@ public class SavingsAccount {
             System.out.printf("%-10s%10s%10s\n", monthNum, saver1.toString(),
                     saver2.toString());
         }
-        
+        /* Resets the interest rate to 5% and calculates month 13. */
         SavingsAccount.setInterestRate(0.05);
         saver1.calculateMonthlyInterest();
         saver2.calculateMonthlyInterest();
